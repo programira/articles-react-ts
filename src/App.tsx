3ce94c56bfae32ef40dc453cc168af58a2f727e0
Mirjana Milosevic <mira.programira@gmail.com>
2021-10-03 16:03:08 +0200

    Installed react, ts, redux, router, mui

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Button variant="contained">Hello World</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
